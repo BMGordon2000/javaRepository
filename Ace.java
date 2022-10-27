28731d2792a5aac5c8770f23ec43cfb6c0d93dc1
BMGordon2000 <98360834+BMGordon2000@users.noreply.github.com>
2022-10-27 00:11:04 -0400

    Delete Ace.java

--- Ace.java
+++
@@ -1,9 +0,0 @@
-package com.example.groupproj_blackjack;
-
-public class Ace extends Card { // Ace class is subclass of Card
-
-    public Ace(Suit suit, int value, String name) { // uses Card suit, value, and name
-        super(suit, 11, "Ace"); // calls from superclass Card and implements these values for Ace class
-    } // closes constructor Ace
-} // closes Ace class
-
